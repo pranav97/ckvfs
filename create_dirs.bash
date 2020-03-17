a472eaa3862ee5653e327214db0af25e5806acac
Pranav Raghavan <gtalkpranav@gmail.com>
2020-03-17 03:32:00 +0000

    tests and shared lib file

---
+++ create_dirs.bash
@@ -0,0 +1,21 @@
+# test 1
+mkdir test_cfs/a_directory
+echo "asdfasdfasdff" > test_cfs/a_directory/aaaa
+echo "asdfasdfasdff" > test_cfs/a_directory/aaaw
+echo "asdfasdfasdff" > test_cfs/a_directory/aaa3
+rm -rf test_cfs/a_directory
+
+
+
+# test 2
+mkdir test_cfs/v_directory
+echo "asdfasdfasdaslk pranav is the greatest" > test_cfs/v_directory/pranav.txt
+rmdir test_cfs/v_directory
+rm -rf test_cfs/v_directory
+
+# test 3
+
+mkdir test_cfs/eee_directory
+echo "asdfasdfasdaslk pranav is the greatest" > test_cfs/eee_directory/pranav.txt
+rm test_cfs/eee_directory/pranav.txt
+rmdir test_cfs/eee_directory
---
+++ create_files.bash
@@ -0,0 +1,18 @@
+echo "test" > test_cfs/a.txt
+echo "test1" > test_cfs/ab.txt
+echo "test2" > test_cfs/ac.txt
+echo "test3" > test_cfs/ad.txt
+echo "test4" > test_cfs/ae.txt
+echo "test5" > test_cfs/af.txt
+echo "test6" > test_cfs/ag.txt
+echo "test7" > test_cfs/ah.txt
+echo "test8" > test_cfs/aj.txt
+cat test_cfs/a.txt
+cat test_cfs/ab.txt
+cat test_cfs/ac.txt
+cat test_cfs/ad.txt
+cat test_cfs/ae.txt
+cat test_cfs/af.txt
+cat test_cfs/ag.txt
+cat test_cfs/ah.txt
+cat test_cfs/aj.txt
